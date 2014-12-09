2b743fa606c951720fbdc1edeabf71bbf301639e
Dennis Daume <daume.dennis@gmail.com>
2014-12-09 18:35:42 +0100

    Imported the ClickOnce uninstaller from https://github.com/6wunderkinder/Wunder.ClickOnceUninstaller

---
+++ ClickOnceToSquirrelMigrator/IUninstallStep.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wunder.ClickOnceUninstaller
+{
+    public interface IUninstallStep : IDisposable
+    {
+        void Prepare(List<string> componentsToRemove);
+
+        void PrintDebugInformation();
+
+        void Execute();
+    }
+}
---
+++ ClickOnceToSquirrelMigrator/UninstallInfo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Win32;
+
+namespace Wunder.ClickOnceUninstaller
+{
+    public class UninstallInfo
+    {
+        public const string UninstallRegistryPath = @"Software\Microsoft\Windows\CurrentVersion\Uninstall";
+
+        private UninstallInfo()
+        {
+        }
+
+        public static UninstallInfo Find(string appName)
+        {
+            var uninstall = Registry.CurrentUser.OpenSubKey(UninstallRegistryPath);
+            if (uninstall != null)
+            {
+                foreach (var app in uninstall.GetSubKeyNames())
+                {
+                    var sub = uninstall.OpenSubKey(app);
+                    if (sub != null && sub.GetValue("DisplayName") as string == appName)
+                    {
+                        return new UninstallInfo
+                                   {
+                                       Key = app,
+                                       UninstallString = sub.GetValue("UninstallString") as string,
+                                       ShortcutFolderName = sub.GetValue("ShortcutFolderName") as string,
+                                       ShortcutSuiteName = sub.GetValue("ShortcutSuiteName") as string,
+                                       ShortcutFileName = sub.GetValue("ShortcutFileName") as string,
+                                       SupportShortcutFileName = sub.GetValue("SupportShortcutFileName") as string,
+                                       Version = sub.GetValue("DisplayVersion") as string
+                                   };
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public string Key { get; set; }
+
+        public string UninstallString { get; private set; }
+
+        public string ShortcutFolderName { get; set; }
+
+        public string ShortcutSuiteName { get; set; }
+
+        public string ShortcutFileName { get; set; }
+
+        public string SupportShortcutFileName { get; set; }
+
+        public string Version { get; set; }
+
+        public string GetPublicKeyToken()
+        {
+            var token = UninstallString.Split(',').First(s => s.Trim().StartsWith("PublicKeyToken=")).Substring(16);
+            if (token.Length != 16) throw new ArgumentException();
+            return token;
+        }
+    }
+}
